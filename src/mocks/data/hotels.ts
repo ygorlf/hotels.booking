9649a6d1c2d8cfe11e00a574f27ace6a524c2ea1
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2023-11-11 23:24:50 -0300

    MSW Config and Data, Hotels Fetch

---
+++ src/mocks/data/hotels.ts
@@ -0,0 +1,152 @@
+export const hotelsData = [
+  {
+    id: '1',
+    name: 'Windsor Marapendi',
+    address: 'Av. Lúcio Costa, 5400 - Barra da Tijuca',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    photo: 'https://thumbcdn-4.hotelurbano.net/KMgrbvNwqGsuE1m5MQ2Gxi8gGrs=/trim:bottom-right:80/bottom/filters:quality(50)/https%3A//novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/1735/windsor-marapendi-hotel-004_20190625114933.jpg',
+    tour: false,
+    breakfast: false,
+    classification: 5,
+    prices: {
+      week: 430,
+      weekend: 362
+    },
+  },
+  {
+    id: '2',
+    name: 'Copacabana Palace',
+    address: 'Av. Atlântica, 1702 - Copacabana',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    photo: 'https://www.correio24horas.com.br/fileadmin/_processed_/d/4/csm_Copacabana_Palace_editada_052afb1d73.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 5,
+    prices: {
+      week: 2072,
+      weekend: 1980
+    },
+  },
+  {
+    id: '3',
+    name: 'Mama Shelter',
+    address: 'Rua Paschoal Carlos Magno, 5 - Santa Teresa',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    photo: 'https://media.gazetadopovo.com.br/viver-bem/2016/03/13228894_1_z-5c238e80.jpg',
+    tour: true,
+    breakfast: false,
+    classification: 4,
+    prices: {
+      week: 358,
+      weekend: 298
+    },
+  },
+  {
+    id: '4',
+    name: 'Buzios Beach Resort',
+    address: 'Rua das Dunas, S/N - Praia de Tucuns',
+    city: 'Búzios',
+    state: 'RJ',
+    photo: 'https://thumbcdn-3.hotelurbano.net/VSWGkUoi11lcRFnbmq9rKakYJL8=/trim:bottom-right:80/bottom/filters:quality(50)/http%3A//media.omnibees.com/Images/5252/Property/115953.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 4,
+    prices: {
+      week: 439,
+      weekend: 702
+    },
+  },
+  {
+    id: '5',
+    name: 'Hotel Laghetto Siena',
+    address: 'Avenida das Hortências, 3000 - R. Vigilante',
+    city: 'Gramado',
+    state: 'RS',
+    photo: 'https://thumbcdn-4.hotelurbano.net/XwZYFPua_uZSeBXKLLBk-gRKfYw=/trim:bottom-right:80/bottom/filters:quality(50)/https%3A//novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/2217/hotel-laghetto-siena-001_20190712121101.jpg',
+    tour: true,
+    breakfast: true,
+    classification: 5,
+    prices: {
+      week: 268,
+      weekend: 365
+    },
+  },
+  {
+    id: '6',
+    name: 'Hotel Ibis Santos Dumont',
+    address: 'Av. Mal. Câmara, 280 - Centro',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    photo: 'https://thumbcdn-2.hotelurbano.net/GHQJdQJD2ZRYj4-rUXtNmvUf55A=/trim:bottom-right:80/bottom/filters:quality(50)/https%3A//novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/524861/001-fachada-ibis-rio-de-janeiro-santos-dumont-001_20191212151900.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 3,
+    prices: {
+      week: 119,
+      weekend: 154
+    },
+  },
+  {
+    id: '7',
+    name: 'Castelo de Itaipava Hotel',
+    address: 'Km 56 ao km 57, 998 BR-040 - Itaipava',
+    city: 'Petrópolis',
+    state: 'RJ',
+    photo: 'https://thumbcdn-3.hotelurbano.net/KELVb-1F3P9ImCTCvvphl64GmlM=/trim:bottom-right:80/bottom/filters:quality(50)/https%3A//novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/2829/castelo-de-itaipava-001_20200310144500.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 4,
+    prices: {
+      week: 90,
+      weekend: 400
+    },
+  },
+  {
+    id: '8',
+    name: 'Pousada da Pedra',
+    address: 'Rua Alecrim, 53A - Jd do Emb.',
+    city: 'Campos do Jordão',
+    state: 'SP',
+    photo: 'https://thumbcdn-1.hotelurbano.net/dDlgPTegjLDd07dCh9Ohr-5-y9I=/trim:bottom-right:80/bottom/filters:quality(50)/http%3A//media.omnibees.com/Images/5647/Property/166303.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 5,
+    prices: {
+      week: 715,
+      weekend: 795
+    },
+  },
+  {
+    id: '9',
+    name: 'Hotel Monte Felice Centro',
+    address: 'Av. das Hortênsias, 2152 - Centro',
+    city: 'Gramado',
+    state: 'RS',
+    photo: 'https://thumbcdn-2.hotelurbano.net/p94sYGJlwt37aMLXICcJmeVAu10=/trim:bottom-right:80/bottom/filters:quality(50)/http%3A//media.omnibees.com/Images/8594/Property/384416.jpg',
+    tour: false,
+    breakfast: true,
+    classification: 3,
+    prices: {
+      week: 246,
+      weekend: 693
+    },
+  },
+  {
+    id: '10',
+    name: 'Casinha Tropical',
+    address: 'Rua Paschoal Carlos Magno, 5 - Santa Teresa',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
+    photo: 'https://thumbcdn-0.hotelurbano.net/bijjNm4_51wMjVQrOjz8LZXWHqk=/trim:bottom-right:80/bottom/filters:quality(50)/https%3A//novo-hu.s3.amazonaws.com/reservas/ota/prod/hotel/756161/275367777_20210618101922.jpg',
+    tour: false,
+    breakfast: false,
+    classification: 4,
+    prices: {
+      week: 73,
+      weekend: 82
+    },
+  },
+];
--- src/pages/booking/index.tsx
+++ src/pages/booking/index.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+
 const Booking = () => {
+  const [hotels, setHotels] = useState([]);
+
+  const fetchHotels = async () => {
+    try {
+      const data = await (await fetch(`${import.meta.env.VITE_API_URL}/hotels`)).json();
+      setHotels(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchHotels();
+  }, []);
+
   return (
     <div>
       Booking Page!
+
+      
     </div>
   )
 };
