8dafaf391063d1a9c97498bde51fa28505376238
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2023-11-14 00:11:19 -0300

    My Books Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,40 @@ import {
 } from 'react-router-dom'
 import './App.css'
 
+import Header from './components/header';
+
 // Pages
 import Home from './pages/home'
 import Book from './pages/book';
+import MyBooks from './pages/myBooks';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <>
+        <Header />
+        <Home />
+      </>
+    ),
   },
   {
     path: "/book/:id",
-    element: <Book />,
+    element: (
+      <>
+        <Header />
+        <Book />
+      </>
+    ),
+  },
+  {
+    path: "/books",
+    element: (
+      <>
+        <Header />
+        <MyBooks />
+      </>
+    ),
   },
 ]);
 
